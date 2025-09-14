5fe80ecc4893a8f7b408027ef06b8b2cc305ea2c
Sebastian Campos Lopez <sebastianclopez@outlook.es>
2025-09-14 14:43:02 -0500

    Se subieron los cambios

---
+++ Frontend/Resources/navbar.js
@@ -0,0 +1,6 @@
+fetch('./Resources/navbar.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('navbar').innerHTML = data;
+  })
+  .catch(error => console.error('Error al cargar navbar:', error));
